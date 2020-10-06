13728d1d092cd3de20cc9d98b6e02a338ac9b584
Dadang Kurnia Sandi <dksm997@gmail.com>
2020-10-06 08:21:55 +0700

    Update 5_1_1521024141_DadangKurniaSandi.php

--- 5_1_1521024141_DadangKurniaSandi.php
+++ 5_1_1521024141_DadangKurniaSandi.php
@@ -6,13 +6,13 @@
 <body><?php
 		$barang = array 
 			(
-				array(1001,"Sabun Lifebuoy","Rp.  1.500,-"),
+				array(1001,"Sabun Lifebuoy","Rp.  1.600,-"),
 				array(1002,"Permen Blaster","Rp.  5.600,-"),
 				array(1003,"Pasta Gigi Pepsodent","Rp.  4.560,-"),
 				array(1004,"Madu Arbain","Rp. 30.000,-"),
 				array(1005,"Kecap ABC","Rp.  7.250,-"),
 				array(1006,"Saus Tomat ABC","Rp.  6.700,-"),
-				array(1007,"Gula Gulaku","Rp.  8.900,-"),
+				array(1007,"Gula Gulaku","Rp.  9.800,-"),
 				array(1008,"Rinso","Rp.  7.100,-"),
 				array(1009,"Super Pel","Rp.  6.450,-"),
 				array(1010,"Permen Tango","Rp.  5.600,-")
